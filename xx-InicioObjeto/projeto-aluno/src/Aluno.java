7107352535c23aa0731794d638a6d2716957d822
Luciano D'Oliveira <106210153+HpBtw@users.noreply.github.com>
2025-02-10 22:36:25 -0300

    Add files via upload

---
+++ xx-InicioObjeto/projeto-aluno/src/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno {
+    // atributos ou propriedades ou variável de instância
+    int rm;
+    String nome;
+    double mediaGeral;
+
+    public static void main(String[] args) {
+        // criação de objeto
+        Aluno objeto = new Aluno();
+        objeto.rm = 1000;
+    }
+}
